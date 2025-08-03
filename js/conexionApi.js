ba5cbed940fbd5939b615dd2be94799a9935d89f
Alejandro <alejodev2022@gmail.com>
2025-08-03 12:55:42 -0500

    Agregar función para verificar la conexión a la API

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -1,3 +1,27 @@
+
+export const  verificarconexiondelaapi = async () => {
+
+  try {
+    const response = await fetch('https://alura-geek-api-six.vercel.app/');
+    if (!response.ok) {
+      throw new Error(`Error en la conexión: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log("Conexión exitosa:", data);
+    return true;
+  } catch (error) {
+    console.error("Error en la conexión:", error);
+    alert("Error en la conexión: " + error.message);
+    return false;
+  }
+
+
+}
+
+
+
+
+
 // Función para obtener la lista de productos
 async function listaProductos() {
   try {
